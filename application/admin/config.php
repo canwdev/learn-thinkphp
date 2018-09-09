9684b66f1908a0f510bd7353c32e535a849610d5
canwdev <c2097@live.com>
2018-09-09 15:28:00 +0800

    模板输出替换

---
+++ application/admin/config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+return [
+    'template'               => [
+
+        'view_suffix'  => 'html',
+    ],
+
+    // 视图输出字符串内容替换
+    'view_replace_str'  =>  [
+        '__PUBLIC__'=>'/public/',
+        '__ADMIN__'=>'/static/admin',
+        '__ROOT__' => '/',
+    ]
+];
